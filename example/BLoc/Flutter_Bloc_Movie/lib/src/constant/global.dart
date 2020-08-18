639be8ec272bdf7d3d55734a8611b37826b457fc
G-Dragon <2359525451@qq.com>
2020-08-18 08:57:37 +0800

    BLoC example

---
+++ example/BLoc/Flutter_Bloc_Movie/lib/src/constant/global.dart
@@ -0,0 +1,6 @@
+class MovieListType {
+  static String popular = "popular"; //流行
+  static String nowPlaying = "now_playing"; //正在播放
+  static String topRated = "top_rated"; //最高评分
+  static String upcoming = "upcoming"; //即将上映
+}
